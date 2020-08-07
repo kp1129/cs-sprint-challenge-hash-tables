1d95944999e4e8b4e5f01c1944e62b04e300019c
kp1129 <katya.p.1129@gmail.com>
2020-08-07 09:34:44 -0700

    solve second problem (ex3), all tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,38 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # a dictionary of element frequencies
+    my_dict = {}
+    # iterate over the array of arrays
+    # for each array, add each of its elements
+    # to the dictionary
+    for arr in arrays:
+        for each in arr:
+            if each in my_dict:
+                my_dict[each] += 1
+            else:
+                my_dict[each] = 1    
+    # at the end, check to see which numbers
+    # in the dictionary have the frequency that
+    # equals the length of the outer array
+    all_sorted = sorted(my_dict.items(), key=lambda pair: pair[1], reverse=True)
+    results = []
+    # while loop setup
+    # length of outer array to make sure we don't go over the index
+    length = len(arrays)
+    # length of all_sorted to make sure we don't go over the index
+    sorted_length = len(all_sorted)
+    # flag to exit the loop when it starts iterating over pairs
+    # that occur at frequencies we don't care about
+    keep_going = True
+    ix = 0
+    while ix < length and ix < sorted_length and keep_going:
+        if all_sorted[ix][1] == length:
+            results.append(all_sorted[ix][0])
+            ix += 1
+        else:
+            keep_going = False    
+    
+    return results
 
-    return result
 
 
 if __name__ == "__main__":
