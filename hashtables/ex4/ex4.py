b0b8de9790e0bfb538dd7875e77a7ae2df275113
kp1129 <katya.p.1129@gmail.com>
2020-08-07 09:49:30 -0700

    solve third problem (ex4), all tests passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,23 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # dictionary to lookup values
+    my_dict = {}
+    # compile the dictionary:
+    # iterate over the list of integers
+    # if a number is negative, add it to dict
+
+    for num in a:
+        if num < 0 and num not in my_dict:
+            my_dict[num] = 1
+
+    # iterate over input and for each positive number
+    # subtract it from 0 and check the dictionary
+    
+    result = []
+    for num in a:
+        if num > 0:
+            complement = 0 - num
+            if complement in my_dict:
+                result.append(num)
 
     return result
 
